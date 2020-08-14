3d0fcc067b58c0f049fd32a7dc9bae16e1ea4959
MrSon <sonhoang.hl97@gmail.com>
2020-08-14 17:23:28 +0700

    HS

--- route/users.route.js
+++ route/users.route.js
@@ -1,40 +1,14 @@
 var express = require('express');
 
 var router = express.Router();
-const shortid = require('shortid');
 
-var db = require('../db')
+var controller = require('./controller/users.controller')
 
-router.get('/', function(req,res){
-	res.render('users/index', {
-		users : db.get('users').value()
-	})
-})
-router.get('/search', function(req,res){
-	var q = req.query.q;
-	var matchedUsers = db.get('users').value().filter(function(user){
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-	})
-	res.render('users/index', {
-		users : matchedUsers
-	})
-})
-router.get('/create', function(req,res){
-	res.render("users/create")
-})
-router.post('/create', function(req,res){
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write()
-	res.redirect("/users")	
-})
-router.get('/:id', function(req,res){
-	var id=req.params.id;
-	var user =db.get('users').find({id :id}).value()
-
-	res.render('users/view',{
-		user : user
-	})
-
-})
+router.get('/', controller.index
+	)
+router.get('/search', controller.search)
+router.get('/create', controller.create)
+router.post('/create', controller.postCreate)
+router.get('/:id', controller.view)
 
 module.exports = router
